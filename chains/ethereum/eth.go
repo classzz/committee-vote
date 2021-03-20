3a785d81db983bcc2ed9489716f40082884dbe7e
Wilson-ton <Wilson-ton@protonmail.com>
2021-03-21 00:15:47 +0800

    update limit

--- chains/ethereum/eth.go
+++ chains/ethereum/eth.go
@@ -69,7 +69,7 @@ func (ec *EthClient) Casting(items *btcjson.ConvertItemsResult) (string, error) 
 	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(3))
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.Value = big.NewInt(0)     // in wei
-	auth.GasLimit = uint64(600000) // in units
+	auth.GasLimit = uint64(800000) // in units
 	auth.GasPrice = gasPrice
 
 	toaddresspuk, err := crypto.DecompressPubkey(items.PubKey)
