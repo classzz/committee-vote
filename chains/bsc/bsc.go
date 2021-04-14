cc48aee14a1b548ed73ece072e7fe108dbd20a32
Wilson-ton <Wilson-ton@protonmail.com>
2021-04-14 14:06:10 +0800

    update bsc

--- chains/bsc/bsc.go
+++ chains/bsc/bsc.go
@@ -69,8 +69,8 @@ func (ec *BscClient) Casting(items *btcjson.ConvertItemsResult) (string, error) 
 
 	auth, _ := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(97))
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0) // in wei
-	//auth.GasLimit = uint64(1200000) // in units
+	auth.Value = big.NewInt(0)      // in wei
+	auth.GasLimit = uint64(1200000) // in units
 	auth.GasPrice = gasPrice
 
 	toaddresspuk, err := crypto.DecompressPubkey(items.PubKey)
