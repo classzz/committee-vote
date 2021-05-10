e24ba111583ab382e6477af9106cf1d9dd5ee2b7
matthew-pro <matthew-proto@protonmail.com>
2021-05-10 18:42:23 +0800

    update paths

--- chains/heco/heco.go
+++ chains/heco/heco.go
@@ -57,7 +57,7 @@ func (ec *HecoClient) Casting(items *btcjson.ConvertItemsResult) (*types.Transac
 		}
 	} else {
 		countSplit[0] = ""
-		countSplit[1] = "0xED7d5F38C79115ca12fe6C0041abb22F0A06C300"
+		countSplit = append(countSplit, "0xED7d5F38C79115ca12fe6C0041abb22F0A06C300")
 	}
 	contractAddress := common.HexToAddress(ec.Cfg.ContractAddress)
 	swaprouter := common.HexToAddress(countSplit[1])
